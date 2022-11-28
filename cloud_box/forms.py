43c115a3d606678690f430e8194e9c668eb2d559
Mytiaiev <mytiaievyevhen@gmail.com>
2022-11-28 16:49:20 +0200

    add landing for reg and login

--- cloud_box/forms.py
+++ cloud_box/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from .models import Document
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = '__all__'
+
+        
+class CloudUser(UserCreationForm):
+  
+    # acces = forms.CharField(max_length=50)
+    
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(CloudUserForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
